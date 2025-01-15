b633cb63e0b50bc6dc0dd2b2ef5cc0f797858c5a
Matt Maher <mattmaher@hotmail.com>
2025-01-15 17:09:44 -0500

    Adding the ODM script from a previous project.

---
+++ Remove-ODMUsers.ps1
@@ -0,0 +1,34 @@
+Import-Module .\ODMAPI
+Connect-OdmService
+Select-OdmOrganization -OrganizationId XXXXXXXXXXXXXXXXXXXXXXXX
+Select-OdmProject 'XXXXXXXXXXXXXXXXXXXXXXX-XX'
+
+#FInd all collections
+$collections = Get-OdmCollection -ResultSize 100
+
+
+#define wave to find
+$wave = "Wave 1"
+
+$UsersToRemove = @(
+"user1@example.com",
+"user2@example.com"
+)
+
+$Users = @()
+foreach($collection in $collections){
+    if($collection.name.contains($wave)){
+        $Users += Get-OdmObject -FilterObject $collection
+    }
+}
+
+foreach($user in $users){
+    if($User.SourceUserPrincipalName -in $UsersToRemove){
+        write-host "Removing "$user.SourceUserPrincipalName -ForegroundColor Green
+        foreach($collection in $collections){
+            if($collection.name.contains($wave)){
+                Remove-OdmObject -From $collection -Objects $user
+            }
+        }
+    }
+}
